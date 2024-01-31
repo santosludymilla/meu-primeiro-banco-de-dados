fb996f027cc5c188fc3c3fb0053c4fb17804d9cb
santosludymilla <156118569+santosludymilla@users.noreply.github.com>
2024-01-31 17:41:19 -0300

    criação de rota

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,34 @@
 import express from "express";
+import {db, firestore} from '../banco-de-dados/firebase';
 
-const app = express();
+const app = express()
 
+app.get('/' ,( req, res) =>{
+    res.send("bem vindo a minha primeira API");
+});
+
+app.post('/usuario', async (req, res) =>{
+    const usuario = req.body.nome
+
+  try{
+     const doRef = await  firestore.addDoc(firestore.collection(db, 'usuarios'),{
+nome:nome,
+     })
+        res.send(docRef.id)
+    } catch (e) {
+        console.log(e)
+
+        res.status(500).send(e)
+    }
+})
+    
 
 
 app.listen(3000,function () {
-    console.log("serviço rodando em http:// localhost:3000");
+       console.log("serviço rodando em http://localhost:3000")
 });
+
+
+
+
+
